20f62e71e9c88be5c1a91a3fc6f3a076bbf2f2b6
RodrigoNeoEx <draja.acc@gmail.com>
2021-05-26 10:02:57 -0300

    function to create img and elements

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function removeLoadingSpan() {
   const loading = document.querySelector('span.loading');
   document.querySelector('body').removeChild(loading);
 }
+
+function createCustomElement(element, className, innerText) {
+  const elem = document.createElement(element);
+  elem.className = className;
+  elem.innerText = innerText;
+  return elem;
+}
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
